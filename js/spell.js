7710d95860c1d2f3e04e6f4c83c6a16ba2df7119
Edward <loewen.edward1521@gmail.com>
2023-06-02 09:09:32 +0200

    adding spells

--- js/spell.js
+++ js/spell.js
@@ -10,7 +10,44 @@ spells = {
         // do nothing
     },
 
-    NOTHINGBUTREALLY : function() {
-        // do nothing
-    }
+    //Erdebeben für jede wand an der ein monster sthet bekommt es 2 schaden
+    Erdbeben : function() {
+        for(let i = 0; i < numTilesWidth; i++) {
+            for(let j = 0; j < numTilesHeight; j++) {
+                let tile = getTile(i,j);
+                if(tile.monster) {
+                    let numWalls = 4 - tile.getAdjacentPassableNeighbors().length;
+                    tile.monster.hit(numWalls * 2);
+                }
+            }
+        }
+
+        shakeAmnount = 10;
+    },
+
+    //Heal für den Spieler
+    HEAL : function() {
+        Math.random() < 0.2 ? player.hp += 2: player.hp++;
+    },
+
+    //Reset Level
+    RRESET : function() {
+        startLevel(Math.min(maxHp, player.hp + 1));
+    },
+
+    //Thunder Storm
+
+    //Tsunami
+
+    //Tornado
+    TORNADO : function() {
+        let tiles = player.tile.getAdjacentPassableNeighbors();
+        for(let i = 0; i < tiles.length; i++) {
+            let tile = tiles[i];
+            if(tile.monster) {
+                tile.monster.hit(1);
+            }
+        }
+    },
+
 }
